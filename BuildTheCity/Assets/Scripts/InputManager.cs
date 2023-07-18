139fd8f6dd18a54cfde50c8a41d9e42577b838e8
rpr12 <85062158+rpr12@users.noreply.github.com>
2023-07-18 11:29:51 +0800

    Redo UI
    The UI was redone, adding new designs and buttons to it.

--- BuildTheCity/Assets/Scripts/InputManager.cs
+++ BuildTheCity/Assets/Scripts/InputManager.cs
@@ -6,42 +6,47 @@ using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
-
-    public LayerMask mouseInputMask;
-    public GameObject buildingPrefab;
-    public int cellSize = 3;
+    public LayerMask mouseInputMask; // filter obj interacted through the mouse
+    public GameObject buildingPrefab; // instantiated prefab of building
+    public int cellSize = 3; // size of each cell in game world
 
     void Update()
     {
-        GetInput();
+        GetInput(); 
     }
 
+    // check for mouse input
     private void GetInput()
-    {
+    {   
+        // check is left mouse clicked & ensure pointer not over UI elements
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
+            // cast ray from camera to mouse position
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            // retrieve hit pos if ray hits an obj
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
             {
+                // calculate nearest grid position
                 Vector3 position = hit.point-transform.position;
                 Debug.Log(CalculateGridPosition(position));
+                // instantiate the building prefab
                 CreatebUilding(CalculateGridPosition(position));
-
             }
-        }
-        
+        }   
     }
 
+    // calculate grid pos based on cellSize
     public Vector3 CalculateGridPosition(Vector3 inputPosition)
     {
         int x = Mathf.FloorToInt((float)inputPosition.x/cellSize);
         int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
         return new Vector3(x * cellSize, 0, z * cellSize);
     }
-
-    private void CreatebUilding(Vector3 gridPosition)
+    
+    // instantiate buildingprefab at specified gridPosition
+    private void Createbuilding(Vector3 gridPosition)
     {
-        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+        Instantiate(bUildingPrefab, gridPosition, Quaternion.identity);
     }
 }
