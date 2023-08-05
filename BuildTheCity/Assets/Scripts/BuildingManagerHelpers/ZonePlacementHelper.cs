a742cc5463d5462b0d976d51dc4e5072fae642c9
rpr12 <85062158+rpr12@users.noreply.github.com>
2023-08-05 10:09:15 +0800

    Zone Placement
    Implemented a zone placement system to handle user input, object pooling, and unit tests.

--- BuildTheCity/Assets/Scripts/BuildingManagerHelpers/ZonePlacementHelper.cs
+++ BuildTheCity/Assets/Scripts/BuildingManagerHelpers/ZonePlacementHelper.cs
@@ -36,9 +36,57 @@ public class ZonePlacementHelper : StructureModificationHelper
         Vector3Int maxPoint = Vector3Int.FloorToInt(endPosition);
 
         ZoneCalculator.PrepareStartAndEndPosition(startPosition, endPosition, ref minPoint, ref maxPoint, mapBottomLeftCorner);
+        HashSet<Vector3Int> newPositionsSet = grid.GetAllPositionsFromTo(minPoint, maxPoint);
+        previousEndPositon = endPosition;
+        ZoneCalculator.CalculateZone(newPositionsSet, structuresToBeModified, gameObjectsToReuse);
+
+        foreach (var positionToPlaceStructure in newPositionsSet)
+        {
+            if (grid.IsCellTaken(positionToPlaceStructure))
+                continue;
+            GameObject structureToAdd = null;
+            if (gameObjectsToReuse.Count > 0)
+            {
+                var gameObjectToReuse = gameObjectsToReuse.Dequeue();
+                gameObjectToReuse.SetActive(true);
+                structureToAdd = placementManager.MoveStructureOnTheMap(positionToPlaceStructure, gameObjectToReuse, structureData.prefab);
+
+            }
+            else
+            {
+                structureToAdd = placementManager.CreateGhostStructure(positionToPlaceStructure, structureData.prefab);
+
+            }
+            structuresToBeModified.Add(positionToPlaceStructure, structureToAdd);
+        }
 
 
     }
 
+    public override void CancleModifications()
+    {
+        base.CancleModifications();
+        ResetZonePlacementHelper();
+    }
+
+    public override void ConfirmModifications()
+    {
+        base.ConfirmModifications();
+        ResetZonePlacementHelper();
+    }
+
+    private void ResetZonePlacementHelper()
+    {
+        placementManager.DestroyStructures(gameObjectsToReuse);
+        gameObjectsToReuse.Clear();
+        startPositionAcquired = false;
+        previousEndPositon = null;
+    }
+
+    public override void StopContinuousPlacement()
+    {
+        startPositionAcquired = false;
+        base.StopContinuousPlacement();
+    }
 
 }
