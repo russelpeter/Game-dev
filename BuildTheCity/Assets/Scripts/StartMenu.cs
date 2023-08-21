b3911d4ef3642d997eb302b5cd4c9b79d713510a
rpr12 <85062158+rpr12@users.noreply.github.com>
2023-08-21 15:51:15 +0800

    Refactored main Menu
    Added a new Main menu

--- BuildTheCity/Assets/Scripts/StartMenu.cs
+++ BuildTheCity/Assets/Scripts/StartMenu.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
-{
-   public void StartGame()
-   {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
-}
+// public class StartMenu : MonoBehaviour
+// {
+//    public void StartGame()
+//    {
+//     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+//    }
+// }
